1e0562556c4347b9faaf7627768bfdddc856f10f
engadnanhossain <engr.adnanhossain@gmail.com>
2023-07-06 14:02:14 +0600

    added redux counter

---
+++ index.js
@@ -0,0 +1,63 @@
+const { createStore } = require('redux');
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+
+const initialCount = {
+    count: 0,
+}
+
+const incrementCount = () => {
+    return {
+        type: INCREMENT,
+    }
+};
+
+const decrementCount = () => {
+    return {
+        type: DECREMENT,
+    }
+};
+
+const resetCount = () => {
+    return {
+        type: RESET,
+    }
+};
+
+const countReducer = (state = initialCount, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1,
+            }
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1,
+            }
+        case RESET:
+            return {
+                ...state,
+                count: 0,
+            }
+
+        default:
+            state;
+    }
+}
+
+
+const store = createStore(countReducer);
+
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch(incrementCount());
+store.dispatch(incrementCount());
+store.dispatch(incrementCount());
+store.dispatch(decrementCount());
+store.dispatch(resetCount());
